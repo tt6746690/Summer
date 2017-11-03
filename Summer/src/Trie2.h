c84c361544fddaea5029527ae76bea15b2a2b866
Mark <peiqi1122@gmail.com>
2017-11-03 16:01:02 -0400

    Types OK, start on member functions

--- Summer/src/Trie2.h
+++ Summer/src/Trie2.h
@@ -6,37 +6,69 @@
 #include <string>
 #include <utility> // pair
 #include <vector>
+#include <array>
 #include <map>
+#include <iostream>
+#include <algorithm>
+
+using std::cout;
+using std::endl; 
+using std::ostream;
+
+constexpr char eol() { return '\n'; }
 
 namespace Summer
 {
 
+
+template<typename CharT>
+auto lex_cmp = [](const std::basic_string<CharT>& x, const std::basic_string<CharT>& y) -> bool {
+    return std::lexicographical_compare(x.begin(), x.end(), y.begin(), y.end());
+};
+
+
 template <typename NodeType>
 struct TrieNodeEdge
 {
-    using UNodePointerT = typename NodeType::UPointerT;
-    std::string         prefix;
-    UNodePointerT       child;
+    typename NodeType::KeyT             prefix;
+    typename NodeType::PointerT         child;
+
+    explicit TrieNodeEdge() : prefix(""), child(nullptr) {}
+    explicit TrieNodeEdge(typename NodeType::KeyT k, typename NodeType::PointerT p) : prefix(k), child(p) {}
+    friend inline bool operator< (const TrieNodeEdge& lhs, const TrieNodeEdge& rhs) { return lex_cmp<NodeType::CharT>(lhs.prefix, rhs.prefix); }
 };
 
-template <typename T>
+
+
+template <typename T, typename CharT>
 class TrieNode
 {
   public:
-    using ValueT                = T;
+    using MappedT               = T;
+    using KeyT                  = std::basic_string<CharT>;
+
     using PointerT              = TrieNode *;
-    using UPointerT             = std::unique_ptr<TrieNode<T>>;
-    using SPointerT             = std::shared_ptr<TrieNode<T>>;
+    using UPointerT             = std::unique_ptr<TrieNode<T, CharT>>;
+    using SPointerT             = std::shared_ptr<TrieNode<T, CharT>>;
     using ReferenceT            = TrieNode &;
     using ConstReferenceT       = const TrieNode &;
-    using EdgeT                 = TrieNodeEdge<TrieNode>;
 
-    T                             value;
+    using EdgeT                 = TrieNodeEdge<TrieNode<T, CharT>>;
+    using EdgesT                = std::vector<EdgeT>;
+
     PointerT                      parent;
-    std::vector<EdgeT>            edges;
+    T                             value;
+    EdgesT                        edges;
 
-    explicit TrieNode() {}
-    explicit TrieNode(PointerT parent, ValueT data) : parent(parent), value(value) {}
+  public:
+    explicit TrieNode() : parent(nullptr), value() {}
+    explicit TrieNode(PointerT parent) : parent(parent) {}    
+    explicit TrieNode(PointerT parent, T value) : parent(parent), value(value) {}
+
+    auto edge_size() const -> typename EdgesT::size_type { return edges.size(); }
+
+    auto add_child(KeyT key) -> PointerT;
+    auto add_child(KeyT key, MappedT value) -> PointerT;
 
     friend bool operator< (const TrieNode &rhs, const TrieNode &lhs);
     friend inline bool operator<=(const TrieNode &rhs, const TrieNode &lhs) { return !(lhs < rhs); }
@@ -46,27 +78,41 @@ class TrieNode
     friend inline bool operator!=(const TrieNode &rhs, const TrieNode &lhs) { return  (rhs < lhs) ||  (lhs < rhs); }
 };
 
-template <typename T>
-bool operator<(const TrieNode<T> &rhs, const TrieNode<T> &lhs)
+
+template<typename T, typename CharT> 
+auto TrieNode<T, CharT>::add_child(KeyT key) -> PointerT 
 {
-    if (rhs.value != lhs.value) return rhs.value < lhs.value;
-    else {
-        if (rhs.parent != lhs.parent) return rhs.parent < lhs.parent;
-        else return rhs.edges < lhs.edges;
-    }
+    PointerT new_node = new TrieNode(this);
+    edges.emplace_back(key, new_node);
+    std::sort(edges.begin(), edges.end());
+    return new_node;
 }
 
-template<typename T>
-class TrieIterator {
-    using NodeT             = TrieNode<T>;
-    using NodePointerT      = typename NodeT::PointerT;
+template<typename T, typename CharT> 
+auto TrieNode<T, CharT>::add_child(KeyT key, MappedT value) -> PointerT 
+{
+    PointerT new_node = new TrieNode(this, value);
+    edges.emplace_back(key, new_node);
+    std::sort(edges.begin(), edges.end());
+    return new_node;
+}
 
-    NodePointerT              ptr_;
-public:
+
+
+
+
+
+template<typename T, typename CharT>
+class TrieIterator {    
     using ValueT            = T;
     using PointerT          = T *;
     using ReferenceT        = ValueT &;
     using ConstReferenceT   = const ValueT &; 
+    using NodeT             = TrieNode<T, CharT>;
+    using NodePointerT      = typename NodeT::PointerT;
+
+    NodePointerT              ptr_;
+public:
 
     explicit TrieIterator() : ptr_(nullptr) { }
     explicit TrieIterator(NodePointerT ptr) : ptr_(ptr) { }
@@ -84,13 +130,13 @@ public:
 
 
 template <typename T, typename CharT = char, typename Compare = std::less<CharT>,
-          typename AllocatorTrait = std::allocator_traits<std::pair<const std::basic_string<CharT>, T>>> 
+          typename Allocator = std::allocator<std::pair<const std::basic_string<CharT>, T>>> 
 class Trie {
 public:
     using KeyT              = std::basic_string<CharT>;
     using KeyCompare        = Compare;
 
-    using AllocatorT        = AllocatorTrait;
+    using AllocatorT        = std::allocator_traits<Allocator>;
     using ValueT            = typename AllocatorT::value_type;
     using ReferenceT        = typename AllocatorT::value_type &;
     using ConstReferenceT   = const typename AllocatorT::value_type &;
@@ -99,14 +145,73 @@ public:
     using SizeT             = typename AllocatorT::size_type;
     using DifferenceT       = typename AllocatorT::difference_type;
 public: 
-    using NodeT             = TrieNode<T>;
+    using NodeT             = TrieNode<T, CharT>;
     using NodePointerT      = typename NodeT::PointerT; 
 
-    using IteratorT         = TrieIterator<T>;
-    using ConstIteratorT    = const TrieIterator<T>;
+    using IteratorT         = TrieIterator<T, CharT>;
+    using ConstIteratorT    = const TrieIterator<T, CharT>;
 private:
-    IteratorT                 end_;
+    NodePointerT              begin_node_ = end_node();
+    NodeT                     end_node_;
+    SizeT                     size_ = 0;
+public: 
+    NodePointerT  end_node()   { return static_cast<NodePointerT>(&end_node_); }
+    NodePointerT &begin_node() { return begin_node_; }
+    NodePointerT  root_node()  { return end_node(); }
+    SizeT         size() const { return size_; }
+
+    explicit Trie() {};
+    ~Trie();
+
+    void destroy(NodePointerT nptr);
+    auto begin() -> IteratorT { return IteratorT(begin_node()); }
+    auto end()   -> IteratorT { return IteratorT(end_node());   }
+    auto begin() const -> ConstIteratorT { return IteratorT(begin_node()); } 
+    auto end()   const -> ConstIteratorT { return IteratorT(end_node()); }
+    auto insert(const ValueT& value) -> IteratorT;
+
+    
 };
+
+
+
+
+template <typename T, typename CharT>
+bool operator<(const TrieNode<T, CharT> &rhs, const TrieNode<T, CharT> &lhs)
+{
+    if (rhs.value != lhs.value) return rhs.value < lhs.value;
+    else {
+        if (rhs.parent != lhs.parent) return rhs.parent < lhs.parent;
+        else return rhs.edges < lhs.edges;
+    }
+}
+
+
+
+template<typename T, typename CharT, typename Compare, typename Allocator> 
+Trie<T, CharT, Compare, Allocator>::~Trie()
+{
+    destroy(root_node());
+}
+
+template<typename T, typename CharT, typename Compare, typename Allocator> 
+void Trie<T, CharT, Compare, Allocator>::destroy(NodePointerT nptr) 
+{
+    if(nptr != nullptr) {
+        for(typename Trie::NodeT::EdgeT& edge : nptr->edges) {
+            destroy(edge.child);
+        }
+    }
+}
+
+template<typename T, typename CharT, typename Compare, typename Allocator> 
+auto Trie<T, CharT, Compare, Allocator>::insert(const ValueT& value) -> IteratorT
+{
+    
+}
+
+
+
 
 
 } // namespace Summer
--- Summer/test/test_Trie2.cpp
+++ Summer/test/test_Trie2.cpp
@@ -4,10 +4,27 @@
 
 #include "Trie2.h"
 
+using std::make_pair;
 using namespace Summer;
 
 TEST_CASE("TrieNode2", "[Trie2]")
 {
 
-    auto t = Trie<int>();
+    struct foo { 
+        int a;
+        int b;
+    };
+
+   
+    SECTION("Initialization") {
+        auto t = Trie<foo>();
+        REQUIRE(t.size() == 0);    
+        REQUIRE(t.begin() == t.end());
+    }
+
+    SECTION("Insertion") {
+        auto t = Trie<int>();
+        t.insert(make_pair("home/", 1));
+    }
+    
 }
